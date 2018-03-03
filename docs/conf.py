3513023161b61908fb036285f5443cbd3add9ccf
Christopher Frederickson <fredericc0@students.rowan.edu>
2018-03-03 16:56:05 -0500

    Remove templates and static from Sphinx config

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ extensions = ['sphinx.ext.autodoc',
     'numpydoc']
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['templates']
+#templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -98,7 +98,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
+#html_static_path = ['static']
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
